d35bc345d991a83b4101477d6f0d378098d39d6b
gibsonbett <gibson.bett@student.moringaschool.com>
2022-07-17 14:22:46 +0300

    add js elements

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,24 @@ function produceReport(){
     event.preventDefault();
 
     let data = [];
+    data[0] = data["shed_A"] = parseInt(document.getElementById("shed_A").value);
+    data[1] = data["shed_B"] = parseInt(document.getElementById("shed_B").value);
+    data[2] = data["shed_C"] = parseInt(document.getElementById("shed_C").value);
+    data[3] = data["shed_D"] = parseInt(document.getElementById("shed_D").value);
 
-    
+   let sumData = data.shed_A + data.shed_B + data.shed_C + data.shed_D;
+
+   document.getElementById("outputData").innerHTML += "<Br>"
+
+   document.getElementById("outputData").innerHTML += "<p>Your production in shed A is " + data.shed_A + " litres per day</p>";
+   document.getElementById("outputData").innerHTML += "<p>Your production in shed B is " + data.shed_B + " litres per day</p>";
+   document.getElementById("outputData").innerHTML += "<p>Your production in shed C is " + data.shed_C + " litres per day</p>";
+   document.getElementById("outputData").innerHTML += "<p>Your production in shed D is " + data.shed_D + " litres per day</p>";
+
+   document.getElementById("outputData").innerHTML += "<p>Your total production per day is " + sumData + " litres per day</p>";
+   
+
+   dailyIncome = sumData * 45;
+
+
 }
